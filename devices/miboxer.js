7bfcb268a6666e8fdb46f61e012220a16314762e
captainbalu-hu <70846365+captainbalu-hu@users.noreply.github.com>
2022-02-13 09:52:30 +0100

    FUT039Z-CCTcontrol-fix-initial
    FUT039Z changed to use the new functions in toZigbee and fromZigbee

--- devices/miboxer.js
+++ devices/miboxer.js
@@ -12,8 +12,8 @@ module.exports = [
         model: 'FUT039Z',
         vendor: 'Miboxer',
         description: 'RGB+CCT LED controller',
-        toZigbee: [tz.on_off, tz.tuya_led_control, tz.tuya_do_not_disturb, tz.tuya_color_power_on_behavior],
-        fromZigbee: [fz.on_off, fz.tuya_led_controller, fz.brightness, fz.ignore_basic_report],
+        toZigbee: [tz.on_off, tz.tuya_led_control_miboxer, tz.tuya_do_not_disturb, tz.tuya_color_power_on_behavior],
+        fromZigbee: [fz.on_off, fz.tuya_led_controller_miboxer, fz.brightness, fz.ignore_basic_report],
         exposes: [e.light_brightness_colortemp_colorhs([153, 500]).removeFeature('color_temp_startup'),
             exposes.binary('do_not_disturb', ea.STATE_SET, true, false)
                 .withDescription('Do not disturb mode'),
